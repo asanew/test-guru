f90de8fda06dfdb0500fc5acc9272ea5bbf7c705
asa <asahome@angelita.ru>
2021-07-18 21:38:50 +0700

    First

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :user as :author
+  has_many :questions
+
   def self.by_category(category_name)
     joins('JOIN categories ON tests.category_id = categories.id').where(
       'categories.title = ?', category_name).order(title: :desc).pluck(:title).to_a
