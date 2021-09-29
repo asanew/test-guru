b5551293a3dbc5abb8ac498a5da182e5fd46c39a
suser ladmin <asa@angelita.ru>
2021-09-29 16:00:12 +0700

    Simple tasks ready

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,10 +6,16 @@ class Test < ApplicationRecord
   has_many :test_results, dependent: :destroy
   has_many :users, through: :test_results
 
-  def self.by_category(category_name)
+  validates :title, presence: true, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :easy, -> { where(level: [0,1]) }
+  scope :medium, -> { where(level: (2..4)) }
+  scope :hard, -> { where(level: (5..Float::INFINITY)) }
+
+  scope :by_category, -> (category_name) {
     joins(:category)
       .where(categories: { title: category_name })
       .order(title: :desc)
-      .pluck(:title)
-  end
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,15 @@ class User < ApplicationRecord
   has_many :tests, through: :test_results
   has_many :own_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
-  def get_tests(test_level)
+  validates :login, presence: true
+
+  scope :user_tests, -> (user,test_level) {
     Test.joins(:test_results)
-      .where(test_results: { user: self})
+      .where(test_results: { user: user})
       .where(level: test_level)
+  }
+
+  def get_tests(test_level)
+    self.class.user_tests(self,test_level)
   end
 end
