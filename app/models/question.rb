9f8374f488c018918633432081711a63a9c06ea1
suser ladmin <asa@angelita.ru>
2021-09-29 16:05:23 +0700

    All validations completed

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :test
 
-  validates :body, presence: true
-
   has_many :answers, dependent: :destroy
+
+  validates :body, presence: true
+  validates :answers, length: { minimum: 1, maximum: 4 }
 end
