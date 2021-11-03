e319b4e3ad486286a85892e2d664c6b1d59c2f02
asa <asahome@angelita.ru>
2021-11-03 18:31:27 +0700

    Questions controller started

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,29 @@
 class QuestionsController < ApplicationController
+  before_action :set_test, only: :index
+
+  def index
+    @questions = @test.questions
+    render inline: "<% @questions.each do |q| %><p><%= q.body %></p><% end %>"
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def set_question
+  end
 end
