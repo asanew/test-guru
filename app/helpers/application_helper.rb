d5a471f283e9c8b631c6fb73b526fbe27b1531ea
suser ladmin <asa@angelita.ru>
2021-11-10 20:11:44 +0700

    footer ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  AUTHOR_NAME = 'Асанов С.А.'.freeze
+  PROJECT_NAME = 'Test GURU'.freeze
+  def current_year
+    Time.current.year
+  end
+
+  def author_name
+    AUTHOR_NAME
+  end
+
+  def github_url(author, repo)
+    link_to PROJECT_NAME, "https://github.com/#{author}/#{repo}"
+  end
 end
