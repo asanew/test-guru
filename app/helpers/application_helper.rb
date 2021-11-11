24d6f6e89b72cbce83e0087437be870e99deae53
suser ladmin <asa@angelita.ru>
2021-11-11 19:51:16 +0700

    clean local store

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   AUTHOR_NAME = 'Асанов С.А.'.freeze
-  PROJECT_NAME = 'Test GURU'.freeze
   def current_year
     Time.current.year
   end
@@ -10,6 +9,6 @@ module ApplicationHelper
   end
 
   def github_url(author, repo)
-    link_to PROJECT_NAME, "https://github.com/#{author}/#{repo}"
+    "https://github.com/#{author}/#{repo}"
   end
 end
